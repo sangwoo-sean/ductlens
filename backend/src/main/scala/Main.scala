c04748df9c11360f7ea066764adf1c734af97889
sangwoo <tkddn3367@gmail.com>
2023-11-25 18:48:15 +0900

    maybe version crash

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -4,13 +4,24 @@ import zio.Console.printLine
 import zio.http.*
 import zio.json.*
 
+final case class ProductAddRequest(name: String, description: String, imageUrl: String, url: String) derives JsonCodec
+
 object Main extends ZIOAppDefault {
+
 
   private val app: HttpApp[ProductRepository] =
     Routes(
       Method.GET / "api" / "products" -> handler(
         ProductRepository.getProducts.map(products => Response.json(products.toJson))
-      )
+      ),
+      Method.POST / "api" / "products" -> handler { (request: Request) =>
+        for {
+          _ <- ZIO.unit
+//          body = request.body // no problem
+          body <- request.body.asString //crash compile
+
+        } yield Response.json("{}")
+      }
     ).toHttpApp @@ Middleware.cors
 
   override def run: ZIO[Environment & ZIOAppArgs & Scope, Any, Any] =
